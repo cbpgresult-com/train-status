13d8522aaeda947896a6ccd08108befb88d0724b
cbpgresult-com <sunflixmovies@gmail.com>
2026-07-01 01:21:06 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+
+    API_KEY: "a61c2cb28dmsh816a4962cd342cfp1a4f90jsn0183cae83615",
+
+    API_HOST: "train-running-api.p.rapidapi.com",
+
+    API_URL: "https://train-running-api.p.rapidapi.com/api/LiveTrainApi"
+
+};
